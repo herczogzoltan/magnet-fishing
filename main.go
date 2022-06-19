58961d4d20c52eafeb1747ca001193231d0f5ebd
Zoltán Herczog <zoltan.herczog@pm.me>
2022-06-19 15:23:58 +0200

    feature(throw) store last click duration

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 		Width:         windowWidth,
 		Height:        windowHeight,
 		Player:        nil,
+		Throw:         nil,
 		ClickDuration: ClickDuration(0),
 	}
 
@@ -56,11 +57,19 @@ type Game struct {
 
 func NewGame(game *Game) {
 	game.Player = NewPlayer()
+	game.Throw = &Throw{}
 }
 
 func (g *Game) Update() error {
 	g.Player.Update(g)
+
+	// Do not reset click duration when we have a value
+	if g.ClickDuration != 0 && inpututil.MouseButtonPressDuration(ebiten.MouseButtonLeft) == 0 {
+		return nil
+	}
+
 	g.ClickDuration = ClickDuration(inpututil.MouseButtonPressDuration(ebiten.MouseButtonLeft))
+
 	return nil
 }
 
@@ -69,7 +78,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	g.Player.Draw(screen)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS()))
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("\n\nMouseClick duration: %d", inpututil.MouseButtonPressDuration(ebiten.MouseButtonLeft)), 0, 0)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("\n\nMouseClick duration: %d", g.ClickDuration), 0, 0)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (windowWidth, windowHeight int) {
