aef03b83e65cecc9c4b79be475a24cd30c78ed07
Zoltán Herczog <zoltan.herczog@pm.me>
2022-06-24 11:54:07 +0200

    refactor(main|game) move game to separate file

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"image/color"
 	_ "image/png"
 	"log"
-	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
 )
@@ -63,41 +58,6 @@ func main() {
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type Game struct {
-	Width  int
-	Height int
-	Player *Player
-	Throw  *Throw
-}
-
-func NewGame(game *Game) {
-	game.Player = NewPlayer()
-	game.Throw = &Throw{}
-}
-
-func (g *Game) Update() error {
-	g.Player.Update(g)
-	g.Throw.Update(g)
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.White)
-	g.Player.Draw(screen)
-
-	text.Draw(screen, "Throwing Accuracy:"+strconv.Itoa(int(g.Throw.Accuracy)), mplusNormalFont, g.Width-270, 30, color.Black)
-
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS()))
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (windowWidth, windowHeight int) {
-	return g.Width, g.Height
-}
-
-func (g *Game) isThrown() bool {
-	return !isPrepareThrow() && g.Throw.Accuracy != 0
 }
 
 func isPrepareThrow() bool {
