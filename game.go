b6c3f46a9ec800e383654a7ca8a0b359396092a9
Zoltán Herczog <zoltan.herczog@pm.me>
2022-06-26 19:59:04 +0200

    feature(game) add startup tutorial

--- game.go
+++ game.go
@@ -16,13 +16,14 @@ const (
 )
 
 type Game struct {
-	Width  int
-	Height int
-	Player *Player
-	Throw  *Throw
-	Magnet *Magnet
-	Found  bool
-	Catch  Catch
+	Width       int
+	Height      int
+	Player      *Player
+	Throw       *Throw
+	Magnet      *Magnet
+	Found       bool
+	Catch       Catch
+	GameStarted bool
 }
 
 func NewGame(game *Game) {
@@ -32,12 +33,17 @@ func NewGame(game *Game) {
 	game.Magnet = NewMagnet()
 	game.Found = false
 	game.Catch = Catch{}
+	game.GameStarted = false
 }
 
 func (g *Game) Update() error {
 	g.Player.Update(g)
 	g.Throw.Update(g)
 	g.Magnet.Update(g)
+
+	if isPrepareThrow() {
+		g.GameStarted = true
+	}
 
 	if g.Magnet.Found {
 		g.Found = true
@@ -58,6 +64,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	text.Draw(screen, "Gold:"+strconv.Itoa(int(g.Player.Gold)), mplusNormalFont, g.Width-270, 30, color.Black)
 	text.Draw(screen, "Throwing Accuracy:"+strconv.Itoa(int(g.Throw.Accuracy)), mplusNormalFont, g.Width-270, 60, color.Black)
+
+	if !g.GameStarted {
+		text.Draw(screen, fmt.Sprintln("1. Click and hold your epic left mouse button to catch"), mplusNormalFont, g.Width/4, g.Height/3-90, color.Black)
+		text.Draw(screen, fmt.Sprintln("2. Check the epic stuff you found"), mplusNormalFont, g.Width/4, g.Height/3-60, color.Black)
+		text.Draw(screen, fmt.Sprintln("3. Spend your epic gold on epic stuff"), mplusNormalFont, g.Width/4, g.Height/3-30, color.Black)
+		text.Draw(screen, fmt.Sprintln("4. Click and hold epicly to catch again"), mplusNormalFont, g.Width/4, g.Height/3, color.Black)
+	}
 
 	if g.Found {
 		text.Draw(screen, fmt.Sprintf("+%d Gold!", g.Catch.Gold), mplusBigFont, g.Width/2, g.Height/3-(66-textFloat), color.Black)
